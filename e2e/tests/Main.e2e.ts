783cbb5a25ae11954d80db7abb2c052d19278cea
Rémi Weng <ixartz@users.noreply.github.com>
2023-05-09 01:29:30 +0200

    test: update e2e after using Expo Router

--- e2e/tests/Main.e2e.ts
+++ e2e/tests/Main.e2e.ts
@@ -3,7 +3,7 @@ import { by, element, expect } from 'detox';
 describe('Main', () => {
   it('should have welcome screen', async () => {
     await expect(
-      element(by.text('Open up App.js to start working on your app!'))
+      element(by.text('Open up App/ to start working on your app!'))
     ).toBeVisible();
   });
 });
